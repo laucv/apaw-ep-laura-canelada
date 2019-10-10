3d126e5b876524c424acf0c30af9407427e074ca
laucv <laura.canelada.velasco@alumnos.upm.es>
2019-10-10 10:27:23 +0200

    Add LibraryResource.java #1

---
+++ src/main/java/es/upm/miw/apaw_ep_themes/api_controllers/LibraryResource.java
@@ -0,0 +1,29 @@
+package es.upm.miw.apaw_ep_themes.api_controllers;
+
+import es.upm.miw.apaw_ep_themes.business_controller.LibraryBusinessController;
+import es.upm.miw.apaw_ep_themes.dtos.LibraryDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(LibraryResource.LIBRARIES)
+public class LibraryResource {
+
+    static final String LIBRARIES = "/libraries";
+
+    private LibraryBusinessController libraryBusinessController;
+
+    @Autowired
+    public  LibraryResource (LibraryBusinessController libraryBusinessController){
+        this.libraryBusinessController = libraryBusinessController;
+    }
+
+    @PostMapping
+    public LibraryDto create(@RequestBody LibraryDto libraryDto){
+        libraryDto.validate();
+        return this.libraryBusinessController.create(libraryDto);
+    }
+}
