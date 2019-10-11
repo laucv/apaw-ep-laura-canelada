582ce34bcab214604777ba926ac0e8e85b5f1455
laucv <laura.canelada.velasco@alumnos.upm.es>
2019-10-11 18:09:59 +0200

    Fixed duplicate code POST book_proposals #8

--- src/main/java/es/upm/miw/apaw_ep_themes/dtos/BookProposalDto.java
+++ src/main/java/es/upm/miw/apaw_ep_themes/dtos/BookProposalDto.java
@@ -25,20 +25,20 @@ public class BookProposalDto {
         this.id = bookProposal.getId();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getAuthor() {
