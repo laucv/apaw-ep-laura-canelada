4224f17468e2b24f44af7c81d11d49f7cf95354d
laucv <laura.canelada.velasco@alumnos.upm.es>
2019-10-15 22:20:26 +0200

    Fixed POST, PUT and PATCH methods #12

--- src/main/java/es/upm/miw/apaw_ep_themes/api_controllers/GenreResource.java
+++ src/main/java/es/upm/miw/apaw_ep_themes/api_controllers/GenreResource.java
@@ -3,6 +3,7 @@ package es.upm.miw.apaw_ep_themes.api_controllers;
 import es.upm.miw.apaw_ep_themes.business_controller.GenreBusinessController;
 import es.upm.miw.apaw_ep_themes.dtos.GenreDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class GenreResource {
         this.genreBusinessController = genreBusinessController;
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public GenreDto create(@RequestBody GenreDto genreDto) {
         genreDto.validate();
         return this.genreBusinessController.create(genreDto);
